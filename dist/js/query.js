0f175bda29d4b33779b250b46eca848134a60b6a
yoko <kionako96@gmail.com>
2022-03-17 13:31:38 +0900

    0317

--- dist/js/query.js
+++ dist/js/query.js
@@ -1,5 +1,28 @@
-window.onload = function () {
-  var doc = document;
-  var inquiry = doc.getElementById('j-answer');
-  console.log(inquiry);
-};
+// よくある質問
+window.addEventListener(
+  'load',
+  () => {
+    var doc = document;
+    // 要素取得
+    var answer = doc.getElementsByClassName('j-answer');
+    var plus = doc.getElementsByClassName('op_plus');
+
+    //クリックイベント
+    doc.addEventListener(
+      'click',
+      (e) => {
+        var el = e.target;
+        //配列作成
+        elements = [].slice.call(plus);
+        // 要素の順番を取得
+        var i = elements.indexOf(el);
+        if (el.matches('.op_plus')) {
+          answer[i].classList.toggle('j-anDis');
+          plus[i].classList.toggle('plus');
+        }
+      },
+      false
+    );
+  },
+  false
+);
